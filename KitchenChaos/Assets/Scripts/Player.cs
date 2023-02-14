d10057c025e80e7fac956659546f78ef1c8dc287
Tevfik <79837579+tevfikkk@users.noreply.github.com>
2023-02-14 16:51:36 +0300

    Animations and animation script

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    public bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -33,9 +35,13 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        // Vector3 means Vector3(0,0,0);
+        isWalking = moveDir != Vector3.zero;
         // Rotate the player to face the direction of movement
         float rotateSpeed = 10f;
         // Slerp is a smooth rotation in terms of spherical interpolation
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
+
+    public bool IsWalking() => isWalking;
 }
